fa88ecec00fb1c79f3764ae41d87e989adb5c3af
PaulllK <59933593+PaulllK@users.noreply.github.com>
2020-08-11 18:44:43 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
  require_once('header.php');
 ?>
 <h1>~Paul Kiraly</h1>
-<h1>~paul.kiraly@digitalexplorer.ro</h1>
+<h1>~write me at kiralypaul6@gmail.com</h1>
 <?php
  require_once('footer.php');
 ?>
